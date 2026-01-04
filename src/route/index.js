fdbdffa8fa7a5d35c3a6a6f80b800783de61f939
vannara <narany321@gmail.com>
2026-01-04 13:59:46 +0700

    dashboard1

--- src/route/index.js
+++ src/route/index.js
@@ -3,7 +3,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
-    
+    path: '/',
+    name: 'Dashboard',
+    component: () => import('../views/Dashboard.vue')
   }
 ]
 const router = createRouter({
